59c7539394f6a53e6f475c7defd53eeed01fbe78
Gilberto <gilbertosa099@outlook.com>
2021-10-25 17:45:19 -0500

    [ADD] webpack config

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports ={
+    mode:'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use:{
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                ],
+            },
+            {
+                test: /\.(png|jp(e*g)|svg|gif)$/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            name:'images/[hash]-[name].[ext]',
+                        }
+                    }
+                ]
+            }
+
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer:{
+        historyApiFallback: true,
+    }
+}
